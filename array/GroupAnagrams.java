c6150c20c25a53f11704ad32efa8ad976fbbe4a5
huaquang5320 <thomasdev@gmail.com>
2026-04-18 15:46:02 +0700

    feat: add Group Anagram solution with special hashmap using int[26] for goruping key approach

---
+++ array/GroupAnagrams.java
@@ -0,0 +1,28 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String s : strs) {
+            // 1. tính key cho s bằng int[26]
+            int[] counts = new int[26];
+            for (int i = 0; i < s.length(); i++) {
+                counts[s.charAt(i) - 'a']++;
+            }
+            // 2. convert key thành String
+            String key = Arrays.toString(counts);
+            // 3. add s vào map theo key
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+        }
+
+        // 4. return tất cả values
+        return new ArrayList<>(map.values());
+    }
+}
